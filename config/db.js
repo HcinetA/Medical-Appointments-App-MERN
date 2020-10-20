c7022b60ff0313858131041c36b8ecfe4c30177c
HcinetA <medamine.hcinet@digietab.tn>
2020-10-20 14:19:44 +0100

    fix

--- config/db.js
+++ config/db.js
@@ -4,7 +4,12 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try {
-await mongoose.connect(db);
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+          });
 
 console.log('mongodb connected...')
     } catch(err) {
