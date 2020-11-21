b8c4f8a2cfa208be2fa8b8def390246d61028357
HcinetA <medamine.hcinet@digietab.tn>
2020-11-21 18:35:21 +0100

    login mta3 zebi

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,6 +5,8 @@ import {
 	REGISTER_FAIL,
 	USER_LOADED,
 	AUTH_ERROR,
+	LOGIN_FAIL,
+	LOGIN_SUCCESS,
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -56,6 +58,7 @@ export const register = ({
 			type: REGISTER_SUCCESS,
 			payload: res.data,
 		});
+		dispatch(loadUser());
 	} catch (err) {
 		const errors = err.response.data.errors;
 		if (errors) {
@@ -63,6 +66,37 @@ export const register = ({
 		}
 		dispatch({
 			type: REGISTER_FAIL,
+		});
+	}
+};
+
+// Login USER
+
+export const login = ({ email, password }) => async (dispatch) => {
+	const config = {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	};
+
+	const body = JSON.stringify({
+		email,
+		password,
+	});
+	try {
+		const res = await axios.post('api/user/login', body, config);
+		dispatch({
+			type: LOGIN_SUCCESS,
+			payload: res.data,
+		});
+		dispatch(loadUser());
+	} catch (err) {
+		const errors = err.response.data.errors;
+		if (errors) {
+			errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+		}
+		dispatch({
+			type: LOGIN_FAIL,
 		});
 	}
 };
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
 	Button,
 	Form,
@@ -7,39 +7,64 @@ import {
 	Message,
 	Segment,
 } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
+const Login = ({ login }) => {
+	const [formData, setFormData] = useState({
+		email: '',
+		password: '',
+	});
+	const { email, password } = formData;
+	const onChange = (e) =>
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	const onSubmit = (e) => {
+		e.preventDefault();
+		login(email, password);
+		console.log(formData);
+	};
+	return (
+		<Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+			<Grid.Column style={{ maxWidth: 450 }}>
+				<Header as='h2' color='teal' textAlign='center'>
+					Log-in to your account
+				</Header>
+				<Form size='large' onSubmit={(e) => onSubmit(e)}>
+					<Segment stacked>
+						<Form.Input
+							fluid
+							icon='user'
+							iconPosition='left'
+							placeholder='E-mail address'
+							name='email'
+							value={email}
+							onChange={(e) => onChange(e)}
+						/>
+						<Form.Input
+							fluid
+							icon='lock'
+							iconPosition='left'
+							placeholder='Password'
+							type='password'
+							name='password'
+							value={password}
+							onChange={(e) => onChange(e)}
+						/>
 
-const Login = () => (
-	<Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-		<Grid.Column style={{ maxWidth: 450 }}>
-			<Header as='h2' color='teal' textAlign='center'>
-				Log-in to your account
-			</Header>
-			<Form size='large'>
-				<Segment stacked>
-					<Form.Input
-						fluid
-						icon='user'
-						iconPosition='left'
-						placeholder='E-mail address'
-					/>
-					<Form.Input
-						fluid
-						icon='lock'
-						iconPosition='left'
-						placeholder='Password'
-						type='password'
-					/>
+						<Button color='teal' fluid size='large'>
+							Login
+						</Button>
+					</Segment>
+				</Form>
+				<Message>
+					New to us? <a href='#'>Sign Up</a>
+				</Message>
+			</Grid.Column>
+		</Grid>
+	);
+};
 
-					<Button color='teal' fluid size='large'>
-						Login
-					</Button>
-				</Segment>
-			</Form>
-			<Message>
-				New to us? <a href='#'>Sign Up</a>
-			</Message>
-		</Grid.Column>
-	</Grid>
-);
-
-export default Login;
+Login.PropTypes = {
+	login: PropTypes.func.isRequired,
+};
+export default connect(null, { login })(Login);
