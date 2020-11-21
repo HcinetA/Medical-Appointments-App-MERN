d6910971beb7479f7fd6d034bb0c83620713a369
javdevV <houssem.missaoui@esprit.tn>
2020-11-21 16:45:46 +0100

    update require

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const User = require('../models/user');
+const User = require('../models/user.model');
 router.get('/', async(req, res) => {
     try {
         const user = await User.findById(req.user.id).select('-password');
