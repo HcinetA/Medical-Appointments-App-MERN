c7a5789ac4b0242fbe970e3cb2d0a91256ede904
javdevV <houssem.missaoui@esprit.tn>
2020-11-22 14:30:19 +0100

    add status flag

--- api/models/appointment.model.js
+++ api/models/appointment.model.js
@@ -3,26 +3,18 @@ const User = require('./user.model').schema;
 const Schema = mongoose.Schema;
 
 const AppointmentSchema = new mongoose.Schema({
-
     daySchedule: { type: String },
-    date: {
-        type: Date,
-        required: false
-    },
+    date: { type: Date, required: false },
     time: { type: String },
     doctor: { type: Schema.Types.ObjectId, ref: 'User' },
     patient: { type: Schema.Types.ObjectId, ref: 'Patient' },
     acte: { type: String },
     notes: { type: String },
-    honoraire: { type: String }
-
+    honoraire: { type: String },
+    status: { type: Boolean, default: false, required: false }
 }, {
     timestamps: true,
     discriminatorKey: 'kind'
 });
-
-
-
-
 
 module.exports = Appointment = mongoose.model('Appointment', AppointmentSchema);
