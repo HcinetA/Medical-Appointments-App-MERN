fb7f2cd531d564b70e2f246a276cc119e40f7f8c
javdevV <houssem.missaoui@esprit.tn>
2020-11-23 13:51:42 +0100

    adding more fields for patient

--- api/routes/appointment.route.js
+++ api/routes/appointment.route.js
@@ -6,128 +6,148 @@ var mongoose = require('mongoose');
 const Patient = require('../models/patient.model');
 const Appointment = require('../models/appointment.model');
 
-
-router.get('/', async(req, res) => {
-    try {
-        const appointments = await Appointment.find().populate('patient', ['name', 'phone']).populate('doctor');
-        res.json(appointments);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error);
-    }
+router.get('/', async (req, res) => {
+  try {
+    const appointments = await Appointment.find()
+      .populate('patient', [
+        'name',
+        'phone',
+        'allergie',
+        'maladie',
+        'antecedent',
+        'city',
+        'habitude',
+        'job',
+        'medication',
+      ])
+      .populate('doctor');
+    res.json(appointments);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 });
 
-router.get('/:id', async(req, res) => {
-    try {
-        const id = req.params.id
-        const appointment = await Appointment.findById(id).populate('patient', ['name', 'phone']).populate('doctor');
-        res.json(appointment);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send(error);
-    }
+router.get('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const appointment = await Appointment.findById(id)
+      .populate('patient', [
+        'name',
+        'phone',
+        'allergie',
+        'maladie',
+        'antecedent',
+        'city',
+        'habitude',
+        'job',
+        'medication',
+      ])
+      .populate('doctor');
+    res.json(appointment);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(error);
+  }
 });
 
-router.get('/patient/:patient_id', async(req, res) => {
-    try {
-        const patient_id = req.params.patient_id;
-        const appointments = await Appointment.find({ patient: patient_id });
-        res.json(appointments);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send(error);
-    }
+router.get('/patient/:patient_id', async (req, res) => {
+  try {
+    const patient_id = req.params.patient_id;
+    const appointments = await Appointment.find({ patient: patient_id });
+    res.json(appointments);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(error);
+  }
 });
 
-router.get('/doctor/:user_id', async(req, res) => {
-    try {
-        const doctor_id = req.params.user_id;
-        const appointments = await Appointment.find({ doctor: doctor_id });
-        res.json(appointments);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send(error);
-    }
+router.get('/doctor/:user_id', async (req, res) => {
+  try {
+    const doctor_id = req.params.user_id;
+    const appointments = await Appointment.find({ doctor: doctor_id });
+    res.json(appointments);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(error);
+  }
 });
 
-router.post('/', async(req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    try {
-        const patient = await Patient.findById(req.body.patient);
-        const app = {
-            daySchedule: req.body.daySchedule,
-            date: req.body.date,
-            time: req.body.time,
-            doctor: req.body.doctor,
-            patient: req.body.patient,
-            acte: req.body.acte,
-            notes_consultation: req.body.notes_consultation,
-            honoraire: req.body.honoraire,
-            motif: req.body.motif
-        }
-        const newAppointment = new Appointment(req.body);
-
-        const appointment = await newAppointment.save();
-        patient.appointments.push(appointment);
-        const saved_patient = await patient.save()
-        res.json(appointment);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error);
-    }
-});
-
-router.put('/:id', async(req, res) => {
-    let responseObject = {
-        sucess: true,
-        updated_element: req.body
+router.post('/', async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+    const patient = await Patient.findById(req.body.patient);
+    const app = {
+      daySchedule: req.body.daySchedule,
+      date: req.body.date,
+      time: req.body.time,
+      doctor: req.body.doctor,
+      patient: req.body.patient,
+      acte: req.body.acte,
+      notes_consultation: req.body.notes_consultation,
+      honoraire: req.body.honoraire,
+      motif: req.body.motif,
     };
-    try {
-        await Appointment.findByIdAndUpdate(req.params.id, req.body);
-        res.send(responseObject);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error)
-    }
-});
-router.patch('/:id', async(req, res) => {
-    let responseObject = {
-        sucess: true,
-        updated_element: req.body
-    };
-    try {
-        await Appointment.findByIdAndUpdate(req.params.id, req.body);
-        res.send(responseObject);
+    const newAppointment = new Appointment(req.body);
 
-    } catch (error) {
-        res.status(500).send(error)
-    }
-})
-
-router.delete('/:id', async(req, res) => {
-    try {
-        const appointment = await Appointment.findById(req.params.id);
-        if (!appointment) {
-            return res.status(404).json({ msg: 'appointment inexistant' });
-        }
-        await Appointment.findOneAndRemove(appointment);
-        return res.json("deleted");
-
-    } catch (error) {
-        res.status(500).send(error)
-
-    }
-});
-router.delete('/', async(req, res) => {
-    try {
-        await Appointment.remove({});
-        return res.json("All deleted");
-    } catch (error) {
-        res.status(500).send(error);
-    }
+    const appointment = await newAppointment.save();
+    patient.appointments.push(appointment);
+    const saved_patient = await patient.save();
+    res.json(appointment);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 });
 
-module.exports = router
+router.put('/:id', async (req, res) => {
+  let responseObject = {
+    sucess: true,
+    updated_element: req.body,
+  };
+  try {
+    await Appointment.findByIdAndUpdate(req.params.id, req.body);
+    res.send(responseObject);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+router.patch('/:id', async (req, res) => {
+  let responseObject = {
+    sucess: true,
+    updated_element: req.body,
+  };
+  try {
+    await Appointment.findByIdAndUpdate(req.params.id, req.body);
+    res.send(responseObject);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const appointment = await Appointment.findById(req.params.id);
+    if (!appointment) {
+      return res.status(404).json({ msg: 'appointment inexistant' });
+    }
+    await Appointment.findOneAndRemove(appointment);
+    return res.json('deleted');
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+router.delete('/', async (req, res) => {
+  try {
+    await Appointment.remove({});
+    return res.json('All deleted');
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
