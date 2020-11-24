ca02cbe6f11cc28857b3295642bc6679b6a30aec
HcinetA <medamine.hcinet@digietab.tn>
2020-11-25 00:43:00 +0100

    .

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,45 @@
 import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+export default class DemoApp extends React.Component {
+	render() {
+		return (
+			<FullCalendar
+				defaultView='dayGridMonth'
+				header={{
+					left: 'prev,next today',
+					center: 'title',
+					right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth',
+				}}
+				plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+				initialView='dayGridMonth'
+				editable={true}
+				selectable={true}
+				selectMirror={true}
+				dayMaxEvents={true}
+				events={[
+					{ title: 'event 1', date: '2020-11-25' },
+					{ title: 'event 2', date: '2019-04-02' },
 
-const dashboard = () => {
-	return <div>Assistante Dashboard</div>;
-};
-
-export default dashboard;
+					{
+						title: 'Meeting',
+						start: '2020-11-12T10:30:00',
+						end: '2020-11-12T12:30:00',
+						color: 'purple', // override!
+						url: 'http://google.com/',
+					},
+					{
+						title: 'Lunch',
+						start: '2020-11-12T12:00:00',
+					},
+					{
+						title: 'Meeting',
+						start: '2020-11-12T14:30:00',
+					},
+				]}
+			/>
+		);
+	}
+}
